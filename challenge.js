43c4a2d663bc40d623a2fa4b3cd14e7c5bd6db29
Blake Tsuzaki <dtsuzaki@scu.edu>
2018-05-04 12:07:36 -0700

    Initial

--- challenge.js
+++ challenge.js
@@ -29,20 +29,29 @@
 // Should return "undefined" for any inputs other 
 // than numerical values
 function add(x, y) {
-    return 0;
+    if (isNaN(x) || isNaN(y)) return undefined
+    return x + y;
 }
 
 // Implement the addition of two numbers (x - y)
 // Should return "undefined" for any inputs other 
 // than numerical values
 function subtract(x, y) {
-    return 0;
+    if (isNaN(x) || isNaN(y)) return undefined
+    return x - y;
 }
 
 // Store even numbers from 0 to "num" into an array.
 // Return an empty array for negative numbers.
 // Return "undefined" for non numerical values.
 function printEvens(num) {
-    return null;
+    if (isNaN(num) || num == null) return undefined
+    var i = 0
+    var nums = []
+    while (i <= num) {
+        nums.push(i)
+        i += 2
+    }
+    return nums;
 }
 
