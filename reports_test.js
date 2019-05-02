5fca6d34740b49cb938a35eb2839d86d527f9c4a
Wesley Wei <wesley631w@gmail.com>
2019-05-02 16:37:33 -0700

    Added coding challenge question and test code.

--- reports_test.js
+++ reports_test.js
@@ -1,66 +1,16 @@
-describe("SUDO Coding Challenge", function() {
+describe('SUDO Coding Challenge', function() {
     var expect = chai.expect;
 
-    describe("add", function() {
-        it("should return the correct property (input: 25, 75)", function() {
-            let sum = 100;
-            let result = add(25, 75);
-            expect(result).to.be.a("number");
-            expect(result).to.eql(sum);
+    describe('reverseString', function() {
+        it('should return the correct property (input: ODUS)', function() {
+            let reversedString = 'SUDO';
+            let result = reverseString('ODUS');
+            expect(result).to.be.a('string');
+            expect(result).to.eql(reversedString);
         });
-        it("should handle non numerical cases (input: 'Sudo', 50)", function() {
-            let sum = 100;
-            let result = add("Sudo", 50);
+        it('should handle non string cases (input: 100)', function() {
+            let result = reverseString(100);
             expect(result).to.be.undefined;
-        });
-    });
-
-    describe("subtract", function() {
-        it("should return the correct property (input: 70, 50)", function() {
-            let difference = 20;
-            let result = subtract(70, 50);
-            expect(difference).to.be.a("number");
-            expect(difference).to.eql(result);
-        });
-        it("should handle non numerical cases (input: 'Sudo', 50)", function() {
-            let sum = 100;
-            let result = subtract("Sudo", 50);
-            expect(result).to.be.undefined;
-        });
-    });
-
-    describe("printEvens", function() {
-        it("should return the correct property (input: 10)", function() {
-            let evenTilTen = [0,2,4,6,8,10];
-            let evens = printEvens(10);
-            expect(evens).to.be.an("array");
-            expect(evens).to.eql(evenTilTen);
-        });
-        it("should return the correct property (input: 0)", function() {
-            let evenTilTen = [0];
-            let evens = printEvens(0);
-            expect(evens).to.be.an("array");
-            expect(evens).to.eql(evenTilTen);
-        });
-        it("should return empty array for negative inputs (input: -10)", function() {
-            let evenTilTen = [];
-            let evens = printEvens(-10);
-            expect(evens).to.be.an("array");
-            expect(evens).to.eql(evenTilTen);
-        });
-        it("should return the correct property (input: 2)", function() {
-            let evenTilTen = [0,2];
-            let evens = printEvens(2);
-            expect(evens).to.be.an("array");
-            expect(evens).to.eql(evenTilTen);
-        });
-        it("should handle non numerical case (input: 'Sudo')", function() {
-            let evens = printEvens("Sudo");
-            expect(evens).to.be.undefined;
-        });
-        it("should handle null inputs (input: null)", function() {
-            let evens = printEvens(null);
-            expect(evens).to.be.undefined;
         });
     });
 });
